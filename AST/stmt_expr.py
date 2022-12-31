0b9952cb40fe51c510a17fb7cb7a03fa9543501c
João Maria Henriques Madeira Pereira <joaompereira07@gmail.com>
2022-12-31 00:11:13 +0000

    no sanitizers

--- AST/stmt_expr.py
+++ AST/stmt_expr.py
@@ -1,6 +1,25 @@
 class Stmt_Expression:
     def __init__(self, expr) -> None:
         self.expr = expr
+        self.sources = []
+        self.sanitizers = []
+    
+    def add_source(self, source) -> None:
+        if source not in self.sources:
+            self.sources.append(source)
+
+    def del_source(self, source) -> None:
+        if source in self.sources:
+            self.sources.remove(source)
+        
+    def get_sources(self) -> list:
+        return self.sources
+    
+    def get_sanitizers(self) -> list:
+        return self.sanitizers
+    
+    def set_sources(self, sources) -> list:
+        self.sources = sources
 
     def __str__(self) -> str:
         return 'Stmt_Expression({})'.format(self.expr)
