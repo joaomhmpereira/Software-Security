ad687d2273a56dd26bb631f8c632346f2c5f9d49
João Maria Henriques Madeira Pereira <joaompereira07@gmail.com>
2023-01-04 20:27:59 +0000

    fix expr_const_fetch

--- AST/expr_const_fetch.py
+++ AST/expr_const_fetch.py
@@ -1,5 +1,8 @@
-class Expr_Const_Fetch:
+from AST.expression import Expression
+
+class Expr_Const_Fetch(Expression):
     def __init__(self, name) -> None:
+        super().__init__()
         self.name = name
 
     def __str__(self) -> str:
